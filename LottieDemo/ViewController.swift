935a3c32b007727ee1d9a9e9e06c4d0fea83f984
Raunak Talwar <rtalwar@expedia.com>
2018-03-26 15:35:35 +0530

    add plane animation.

--- LottieDemo/ViewController.swift
+++ LottieDemo/ViewController.swift
@@ -15,12 +15,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-         let animationView = LOTAnimationView(name: "servishero_loading")
-            animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
-            animationView.center = self.view.center
-            animationView.contentMode = .scaleAspectFill
-            view.addSubview(animationView)
-            animationView.play()
+        let animationView = LOTAnimationView(name: "Whiteplane")
+        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFill
+        animationView.loopAnimation = true
+        animationView.animationSpeed = 0.5
+
+        // Applying UIView animation
+        let minimizeTransform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        animationView.transform = minimizeTransform
+        UIView.animate(withDuration: 3.0, delay: 0.0, options: [.autoreverse, .repeat], animations: {
+            animationView.transform = CGAffineTransform.identity
+        }, completion: nil)
+
+//        let rotateTransform = CGAffineTransform(rotationAngle: 45.0)
+//        animationView.transform = rotateTransform
+        view.addSubview(animationView)
+        animationView.play()
         
         
     }
